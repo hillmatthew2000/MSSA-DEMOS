16942ed72fd5de5d9b2abd90c98b5635d3865f73
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-07-24 12:15:10 -0400

    Update Tester

--- CSharp/Tester/Program.cs
+++ CSharp/Tester/Program.cs
@@ -4,8 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        bool isLoggedIn = false;
-        if (!isLoggedIn)
-        Console.WriteLine("log in first");
+        Random coin = new Random();
+        int coinToss = coin.Next(1, 3);
+        Console.WriteLine($"You flipped: {(coinToss == 1 ? "Heads" : "Tails")}!");
+        // Person p1 = new Person("Bob");
+        // Person p2 = new Person("Bob");
+        // Person p3 = p1;
+
+        // string s1 = "hello";
+        // string s2 = "hello";
+        // string s3 = new string("hello".ToCharArray());
+        // Console.WriteLine(s1 == s2);
+        // System.Console.WriteLine(s1 == s3);
+
+        // bool isLoggedIn = false;
+        // if (!isLoggedIn)
+        // Console.WriteLine("log in first");
+
+        // int score = 85;
+        // Console.WriteLine($"Grade: {(score >= 90 ? "A" : score > 80 ? "B" : "C")}");
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public Person(string name)
+    {
+        this.Name = name;
     }
 }
--- CSharp/Tester/obj/Debug/net8.0/Tester.AssemblyInfo.cs
+++ CSharp/Tester/obj/Debug/net8.0/Tester.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tester")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c8bf480dc8015a5a4ba20004ca316af00e4daa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45359a4b6e401ac4eeec894da44fd027084077bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tester")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tester")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
