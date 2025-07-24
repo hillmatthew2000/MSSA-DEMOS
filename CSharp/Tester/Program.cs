eb952260aa0c4d020833b939472c027a64885784
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-07-24 16:56:20 -0400

    update tester

--- CSharp/Tester/Program.cs
+++ CSharp/Tester/Program.cs
@@ -8,66 +8,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        // SKU = Stock Keeping Unit.
-        // SKU value format: <product #>-<2-letter color code>-<size code>
-        string Sku = "01-MN-L";
+        Console.WriteLine("Enter the grid dimension");
+        string userInput = Console.ReadLine();
+        int gridDimensions = int.Parse(userInput);
 
-        string[] product = Sku.Split('-');
-
-        string type = "";
-        string color = "";
-        string size = "";
-
-        switch (product[0])
-        {
-            case "01":
-                type = "Sweat shirt";
-                break;
-            case "02":
-                type = "T-Shirt";
-                break;
-            case "03":
-                type = "Sweat pants";
-                break;
-            default:
-                type = "Other";
-                break;
-        }
-
-        switch (product[1])
-        {
-            case "MN":
-                color = "Mint";
-                break;
-            case "RD":
-                color = "Red";
-                break;
-            case "BL":
-                color = "Blue";
-                break;
-            default:
-                color = "Other";
-                break;
-        }
-
-        switch (product[2])
-        {
-            case "S":
-                size = "Small";
-                break;
-            case "M":
-                size = "Medium";
-                break;
-            case "L":
-                size = "Large";
-                break;
-            default:
-                size = "One Size Fits All";
-                break;
-        }
-
-        Console.WriteLine($"Product: {size} {color} {type}");
-
+        CreateGrid(gridDimensions);
     }
+
+    public static void CreateGrid(int gridDimensions)
+    {
+        char xLetter = 'X';
+        char oLetter = 'O';
+        for (int i = 0; i < gridDimensions; i++)
+        {
+            for (int j = 0; j < gridDimensions; j++)
+            {
+                if ((i + j) % 2 == 0)
+                {
+                    Console.Write(xLetter);
+                }
+                else
+                {
+                    Console.Write(oLetter);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+
 
 }
