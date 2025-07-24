755bc0721048113596795fe569e08f817a23384d
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-07-24 15:49:18 -0400

    Update Tester

--- CSharp/Tester/Program.cs
+++ CSharp/Tester/Program.cs
@@ -8,38 +8,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        int studentGradeLevel = 2;
-        string studentName = "John";
+        // SKU = Stock Keeping Unit.
+        // SKU value format: <product #>-<2-letter color code>-<size code>
+        string Sku = "01-MN-L";
 
-        string gradeDescription = "";
-        switch (studentGradeLevel)
+        string[] product = Sku.Split('-');
+
+        string type = "";
+        string color = "";
+        string size = "";
+
+        switch (product[0])
         {
-            case 1:
-                {
-                    gradeDescription = "Freshman";
-                    break;
-                }
-            case 2:
-                {
-                    gradeDescription = "Sophomore";
-                    break;
-                }
-            case 3:
-                {
-                    gradeDescription = "Junior";
-                    break;
-                }
-            case 4:
-                {
-                    gradeDescription = "Senior";
-                    break;
-                }
+            case "01":
+                type = "Sweat shirt";
+                break;
+            case "02":
+                type = "T-Shirt";
+                break;
+            case "03":
+                type = "Sweat pants";
+                break;
             default:
-                {
-                    gradeDescription = "Unspecified Grade Level";
-                    break;
-                }
-        Console.WriteLine($"Student Name: {studentName}, Grade Level: {gradeDescription}");
+                type = "Other";
+                break;
         }
+
+        if (product[1] == "RD")
+        {
+            color = "Red";
+        }
+        else if (product[1] == "BL")
+        {
+            color = "Black";
+        }
+        else if (product[1] == "MN")
+        {
+            color = "Maroon";
+        }
+        else
+        {
+            color = "White";
+        }
+
+        if (product[2] == "S")
+        {
+            size = "Small";
+        }
+        else if (product[2] == "M")
+        {
+            size = "Medium";
+        }
+        else if (product[2] == "L")
+        {
+            size = "Large";
+        }
+        else
+        {
+            size = "One Size Fits All";
+        }
+
+        Console.WriteLine($"Product: {size} {color} {type}");
+
     }
+
 }
