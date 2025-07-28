55fa53445e8b48c7f2936379e5917b0740a71a7b
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-07-28 17:09:03 -0400

    Update Tester

---
+++ CSharp/HeroGame/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace HeroGame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Enter your Hero's name!");
+        string heroName = Console.ReadLine();
+
+        Console.WriteLine("Enter the Monster's name!");
+        string monsterName = (Console.ReadLine());
+
+        Console.Clear();
+        
+        int heroHealth = 10;
+        int monsterHealth = 10;
+        Random attackvalue = new Random();
+
+        do
+        {
+            int heroAttack = attackvalue.Next(1, 5);
+            monsterHealth -= heroAttack;
+            Console.WriteLine($"{heroName} has struck {monsterName} for {heroAttack} damage! {monsterName} has {monsterHealth} remaining health!");
+
+            if (monsterHealth <= 0)
+                break;
+
+            int monsterAttack = attackvalue.Next(1, 5);
+            heroHealth -= monsterAttack;
+            Console.WriteLine($"{monsterName} has struck {heroName} for {monsterAttack} damage! {heroName} has {heroHealth} remaining health!");
+        } while (heroHealth > 0 && monsterHealth > 0);
+
+        if (heroHealth > 0)
+        {
+            Console.WriteLine($"{heroName} has prevailed!!!");
+        }
+        else
+        {
+            Console.WriteLine($"{monsterName} has vanquished you!");
+        }
+    }
+}
--- CSharp/Tester/Program.cs
+++ CSharp/Tester/Program.cs
@@ -1,28 +1,58 @@
-﻿using System.Buffers;
-using System.Net;
-using System.Threading.Channels;
-
-namespace Tester;
+﻿namespace Tester;
 
 class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 1; i <= 100; i++)
+        // //Variables
+        // string? input;
+        // int result;
+        // bool isValid;
+
+        // //Do-While for checking user input and assigning it to result var
+        // do
+        // {
+        //     Console.WriteLine("Enter a number");
+        //     input = Console.ReadLine();
+        //     isValid = int.TryParse(input, out result);
+        // } while (!isValid || result < 5 || result > 10);
+
+        // Console.Clear();
+        // Console.WriteLine($"{result} is a valid number!");
+
+
+        string? roleName = Console.ReadLine();
+
+        // do
+        // {
+        //     Console.WriteLine("Enter your role name");
+        //     roleName = Console.ReadLine().Trim().ToLower();
+        // } while ((roleName != "administrator") &&
+        //  (roleName != "manager") && (roleName != "user"));
+
+        // Console.Clear();
+        // Console.WriteLine($"Access Granted, Welcome {roleName}!");
+
+        string[] myString = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+        
+        foreach (string sentence in myString)
         {
-            if ((i % 3 == 0) && (i % 5 == 0))
+            string remaining = sentence;
+            int periodLocation;
+            do
             {
-                Console.WriteLine($"{i} FizzBuzz");
-            }
-            else if (i % 3 == 0)
-            {
-                Console.WriteLine($"{i} Fizz");
-            }
-            else if (i % 5 == 0)
-            {
-                Console.WriteLine($"{i} Buzz");
-            }
+                periodLocation = remaining.IndexOf(".");
+                if (periodLocation > -1)
+                {
+                    Console.WriteLine("Sentence: " + remaining.Remove(periodLocation));
+                    remaining = remaining.Substring(periodLocation + 1).TrimStart(' ');
+                }
+                else if (remaining.Length > 0)
+                {
+                    Console.WriteLine("Sentence: " + remaining.TrimStart());
+                }
+                
+            } while (periodLocation > -1);
         }
     }
-
 }
