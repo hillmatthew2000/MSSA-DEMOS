39b43400e93b52de501e088ac91a76788b32897d
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-07-29 17:03:25 -0400

    Add DoWhileExercises

---
+++ CSharp/DoWhileExercises/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Tester;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Variables
+        string? input;
+        int result;
+        bool isValid;
+
+        //Do-While for checking user input and assigning it to result var
+        do
+        {
+            Console.WriteLine("Enter a number");
+            input = Console.ReadLine();
+            isValid = int.TryParse(input, out result);
+        } while (!isValid || result < 5 || result > 10);
+
+        Console.Clear();
+        Console.WriteLine($"{result} is a valid number!");
+
+
+        // string? roleName = Console.ReadLine();
+
+        // do
+        // {
+        //     Console.WriteLine("Enter your role name");
+        //     roleName = Console.ReadLine().Trim().ToLower();
+        // } while ((roleName != "administrator") &&
+        //  (roleName != "manager") && (roleName != "user"));
+
+        // Console.Clear();
+        // Console.WriteLine($"Access Granted, Welcome {roleName}!");
+
+        // string[] myString = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+        
+        // foreach (string sentence in myString)
+        // {
+        //     string remaining = sentence;
+        //     int periodLocation;
+        //     do
+        //     {
+        //         periodLocation = remaining.IndexOf(".");
+        //         if (periodLocation > -1)
+        //         {
+        //             Console.WriteLine("Sentence: " + remaining.Remove(periodLocation));
+        //             remaining = remaining.Substring(periodLocation + 1).TrimStart(' ');
+        //         }
+        //         else if (remaining.Length > 0)
+        //         {
+        //             Console.WriteLine("Sentence: " + remaining.TrimStart());
+        //         }
+                
+        //     } while (periodLocation > -1);
+        // }
+    }
+}
--- CSharp/Tester/Program.cs
+++ CSharp/Tester/Program.cs
@@ -1,58 +1,138 @@
-﻿namespace Tester;
+﻿using System.Reflection;
+
+namespace ConsoleApp1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // //Variables
-        // string? input;
-        // int result;
-        // bool isValid;
+        // the ourAnimals array will store the following: 
+        string animalSpecies = "";
+        string animalID = "";
+        string animalAge = "";
+        string animalPhysicalDescription = "";
+        string animalPersonalityDescription = "";
+        string animalNickname = "";
 
-        // //Do-While for checking user input and assigning it to result var
-        // do
-        // {
-        //     Console.WriteLine("Enter a number");
-        //     input = Console.ReadLine();
-        //     isValid = int.TryParse(input, out result);
-        // } while (!isValid || result < 5 || result > 10);
+        // variables that support data entry
+        int maxPets = 8;
+        string? readResult;
+        string menuSelection = "";
 
-        // Console.Clear();
-        // Console.WriteLine($"{result} is a valid number!");
+        // array used to store runtime data, there is no persisted data
+        string[,] ourAnimals = new string[maxPets, 6];
 
-
-        string? roleName = Console.ReadLine();
-
-        // do
-        // {
-        //     Console.WriteLine("Enter your role name");
-        //     roleName = Console.ReadLine().Trim().ToLower();
-        // } while ((roleName != "administrator") &&
-        //  (roleName != "manager") && (roleName != "user"));
-
-        // Console.Clear();
-        // Console.WriteLine($"Access Granted, Welcome {roleName}!");
-
-        string[] myString = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
-        
-        foreach (string sentence in myString)
+        // create some initial ourAnimals array entries
+        for (int i = 0; i < maxPets; i++)
         {
-            string remaining = sentence;
-            int periodLocation;
-            do
+            switch (i)
             {
-                periodLocation = remaining.IndexOf(".");
-                if (periodLocation > -1)
-                {
-                    Console.WriteLine("Sentence: " + remaining.Remove(periodLocation));
-                    remaining = remaining.Substring(periodLocation + 1).TrimStart(' ');
-                }
-                else if (remaining.Length > 0)
-                {
-                    Console.WriteLine("Sentence: " + remaining.TrimStart());
-                }
-                
-            } while (periodLocation > -1);
+            case 0:
+            {
+                animalSpecies = "dog";
+                animalID = "d1";
+                animalAge = "2";
+                animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
+                animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+                animalNickname = "lola";
+                break;
+            }
+            case 1:
+            {
+                animalSpecies = "dog";
+                animalID = "d2";
+                animalAge = "9";
+                animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+                animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+                animalNickname = "loki";
+                break;
+            }
+            case 2:
+            {
+                animalSpecies = "cat";
+                animalID = "c3";
+                animalAge = "1";
+                animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
+                animalPersonalityDescription = "friendly";
+                animalNickname = "Puss";
+                break;
+            }
+            case 3:
+            {
+                animalSpecies = "cat";
+                animalID = "c4";
+                animalAge = "?";
+                animalPhysicalDescription = "";
+                animalPersonalityDescription = "";
+                animalNickname = "";
+                break;
+            }
+            case 4:
+            {
+                animalSpecies = "";
+                animalID = "";
+                animalAge = "";
+                animalPhysicalDescription = "";
+                animalPersonalityDescription = "";
+                animalNickname = "";
+                break;
+            }
+            }
+
+            ourAnimals[i, 0] = "ID #: " + animalID;
+            ourAnimals[i, 1] = "Species: " + animalSpecies;
+            ourAnimals[i, 2] = "Age: " + animalAge;
+            ourAnimals[i, 3] = "Nickname: " + animalNickname;
+            ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
+            ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
         }
+
+        // display the top-level menu options
+        do
+        {
+            Console.Clear();
+
+            Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
+            Console.WriteLine(" 1. List all of our current pet information");
+            Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
+            Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
+            Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
+            Console.WriteLine(" 5. Edit an animal’s age");
+            Console.WriteLine(" 6. Edit an animal’s personality description");
+            Console.WriteLine(" 7. Display all cats with a specified characteristic");
+            Console.WriteLine(" 8. Display all dogs with a specified characteristic");
+            Console.WriteLine();
+            Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+            // display the top-level menu options
+
+            Console.Clear();
+
+            Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
+            Console.WriteLine(" 1. List all of our current pet information");
+            Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
+            Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
+            Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
+            Console.WriteLine(" 5. Edit an animal’s age");
+            Console.WriteLine(" 6. Edit an animal’s personality description");
+            Console.WriteLine(" 7. Display all cats with a specified characteristic");
+            Console.WriteLine(" 8. Display all dogs with a specified characteristic");
+            Console.WriteLine();
+            Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+
+
+            
+            readResult = Console.ReadLine();
+            if (readResult != null)
+            {
+                menuSelection = readResult.ToLower();
+            }
+
+            Console.WriteLine($"You selected menu option {menuSelection}.");
+            Console.WriteLine("Press the Enter key to continue");
+
+            // pause code execution
+            readResult = Console.ReadLine();
+
+        } while (menuSelection != "exit");
     }
 }
