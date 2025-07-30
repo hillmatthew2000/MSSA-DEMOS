98e1be808655f70cfb3de32d5c0d9aae5bd63833
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-07-30 09:56:20 -0400

    Update DoWhileExercises

--- CSharp/DoWhileExercises/Program.cs
+++ CSharp/DoWhileExercises/Program.cs
@@ -5,23 +5,22 @@ class Program
     static void Main(string[] args)
     {
         //Variables
-        string? input;
-        int result;
-        bool isValid;
+        // string? input;
+        // int result;
+        // bool isValid;
 
         //Do-While for checking user input and assigning it to result var
-        do
-        {
-            Console.WriteLine("Enter a number");
-            input = Console.ReadLine();
-            isValid = int.TryParse(input, out result);
-        } while (!isValid || result < 5 || result > 10);
+        // do
+        // {
+        //     Console.WriteLine("Enter a number");
+        //     input = Console.ReadLine();
+        //     isValid = int.TryParse(input, out result);
+        // } while (!isValid || result < 5 || result > 10);
 
-        Console.Clear();
-        Console.WriteLine($"{result} is a valid number!");
+        // Console.Clear();
+        // Console.WriteLine($"{result} is a valid number!");
 
-
-        // string? roleName = Console.ReadLine();
+        // string? roleName;
 
         // do
         // {
@@ -33,26 +32,26 @@ class Program
         // Console.Clear();
         // Console.WriteLine($"Access Granted, Welcome {roleName}!");
 
-        // string[] myString = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+        string[] myString = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
         
-        // foreach (string sentence in myString)
-        // {
-        //     string remaining = sentence;
-        //     int periodLocation;
-        //     do
-        //     {
-        //         periodLocation = remaining.IndexOf(".");
-        //         if (periodLocation > -1)
-        //         {
-        //             Console.WriteLine("Sentence: " + remaining.Remove(periodLocation));
-        //             remaining = remaining.Substring(periodLocation + 1).TrimStart(' ');
-        //         }
-        //         else if (remaining.Length > 0)
-        //         {
-        //             Console.WriteLine("Sentence: " + remaining.TrimStart());
-        //         }
+        foreach (string sentence in myString)
+        {
+            string remaining = sentence;
+            int periodLocation;
+            do
+            {
+                periodLocation = remaining.IndexOf(".");
+                if (periodLocation > -1)
+                {
+                    Console.WriteLine("Sentence: " + remaining.Remove(periodLocation));
+                    remaining = remaining.Substring(periodLocation + 1).TrimStart(' ');
+                }
+                else if (remaining.Length > 0)
+                {
+                    Console.WriteLine("Sentence: " + remaining.TrimStart());
+                }
                 
-        //     } while (periodLocation > -1);
-        // }
+            } while (periodLocation > -1);
+        }
     }
 }
