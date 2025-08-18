c67dc8f5c4ca304d7bb863b942389bf50021fb9a
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-18 10:20:05 -0500

    Add GUID, Interfaces, and VetCV2

---
+++ CSharp/GUIDConstatsInit/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace GUIDConstantsInit;
+
+class Program
+{
+    const decimal TaxRate = 0.075m;
+    static void Main()
+    {
+        var e1 = new Example();
+        var e2 = new Example();
+
+        Console.WriteLine($"e1.Id: {e1.Id}");
+        Console.WriteLine($"e2.Id: {e2.Id}");
+
+        var p1 = new PersonWithSet { Name = "Alice" };
+        Console.WriteLine($"p1.Name: {p1.Name}");
+        p1.Name = "Bob";
+        Console.WriteLine($"p1.Name: {p1.Name}");
+        var p2 = new PersonWithInit { Name = "Karen" };
+        Console.WriteLine($"p2.Name: {p2.Name}");
+        p2.Name = "Lily"; // This will cause a compile-time error
+        Console.WriteLine($"p2.Name: {p2.Name}");
+    }
+ 
+}
+
+public class Example
+{
+    public readonly Guid Id = Guid.NewGuid();
+
+    public Example()
+    {
+
+    }
+}
+
+public class PersonWithSet
+{
+    public string Name { get; set; }
+}
+
+public class PersonWithInit
+{
+    public string Name { get; init; }
+}
---
+++ CSharp/InterfacesDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace InterfacesDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+interface IPersonName
+{
+    string FirstName { get; set; }
+    string LastName { get; set; }
+    void GetFullName();
+
+}
+
+public class CustomPerson : IPersonName
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public void GetFullName()
+    {
+        Console.WriteLine($"Full Name: {FirstName} {LastName}");
+    }
+}
