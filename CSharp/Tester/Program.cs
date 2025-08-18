3c3cb676623a78011580ff102af3ae3a578d606d
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-18 10:21:13 -0500

    Update Tester

--- CSharp/Tester/Program.cs
+++ CSharp/Tester/Program.cs
@@ -7,21 +7,3 @@ class Program
 
     }
 }
-
-interface IPersonName
-{
-    string FirstName { get; set; }
-    string LastName { get; set; }
-    void GetFullName();
-
-}
-
-public class CustomPerson : IPersonName
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public void GetFullName()
-    {
-        Console.WriteLine($"Full Name: {FirstName} {LastName}");
-    }
-}
