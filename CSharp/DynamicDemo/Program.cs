40b0636134fff03f2e3d7561bb3a419c8961679b
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-19 12:53:14 -0500

    Update Dynamic Demo

--- CSharp/DynamicDemo/Program.cs
+++ CSharp/DynamicDemo/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        dynamic message = "Hello world";
+        System.Console.WriteLine(message.ToUpper());
+
+        message = 123;
+        Console.WriteLine(message + 1); //this displays 124
+        Console.WriteLine(message.ToUpper()); //this will compile however an undhandled exception is thrown
+
     }
 }
