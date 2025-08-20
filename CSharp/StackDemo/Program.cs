23bc9d2d26ebcac40db1fb91a0f3381211a457f5
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-20 10:42:47 -0500

    Add StackDemo

---
+++ CSharp/StackDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace StackDemo;
+
+class Program
+{
+    static void Main()
+    {
+        Stack pageStack = new Stack();
+ 
+        pageStack.Push("Home");
+        pageStack.Push("Products");
+        pageStack.Push("Cart");
+ 
+        Console.WriteLine("Current stack (top to bottom:)");
+        foreach (var page in pageStack)
+        {
+            Console.WriteLine(page);
+        }
+ 
+        Console.WriteLine($"\nCurrent page (top of stack): {pageStack.Peek()}");
+ 
+        string previousPage = (string)pageStack.Pop();
+        Console.WriteLine($"\nBack to: {previousPage}");
+ 
+        Console.WriteLine("Stack after popping");
+        foreach (var page in pageStack)
+        {
+            Console.WriteLine(page);
+        }
+ 
+    }
+ 
+}
