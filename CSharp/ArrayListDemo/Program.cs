ff38ab9fc2c8f0ec4577b7a558b53eee820e88e2
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-20 09:45:54 -0500

    Add ArrayListDemo

---
+++ CSharp/ArrayListDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Net.Quic;
+
+namespace ArrayListDemo;
+
+class Program
+{
+    static void Main()
+    {
+        ArrayList list = new ArrayList();
+        list.Add("Apple");
+        list.Add("Banana");
+        list.Add(123);
+        list.Add(true);
+
+        Console.WriteLine("Access by index:");
+        Console.WriteLine("First item: " + list[0]);
+        Console.WriteLine("Second item: " + list[1]);
+
+        list[1] = "Orange";
+        list.Remove("Apple");
+        list.Insert(1, "Grapes");
+
+        Console.WriteLine("\nCurrentArrayListContents");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine($"Item at index {i}: {list[i]}");
+        }
+
+        list.RemoveAt(1);
+
+        Console.WriteLine("\nAfter RemoveAt(1)");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine($"Item at index {i}: {list[i]}");
+        }
+    }
+}
