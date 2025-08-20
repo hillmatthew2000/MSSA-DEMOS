c532dbfb10cd6b0609a5dfdcf7d6871dca5857a2
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-20 09:50:46 -0500

    Add BitArrayDemo

---
+++ CSharp/BitArrayDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace BitArrayDemo;
+
+class Program
+{
+    static void Main()
+    {
+        BitArray bits = new BitArray(5);
+
+        bits[0] = true;
+        bits[2] = true;
+
+        Console.WriteLine("BitArray values:");
+        for (int i = 0; i < bits.Count; i++)
+        {
+            Console.WriteLine($"Bit {i}: {bits[i]}");
+        }
+
+        bits.Not();
+        Console.WriteLine("\nAfter NOT operation:");
+        for (int i = 0; i < bits.Count; i++)
+        {
+            Console.WriteLine($"Bit {i}: {bits[i]}");
+        }
+    }
+}
