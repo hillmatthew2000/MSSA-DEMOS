e45824ef90d18a6c2e97b0e2173718eb9c9da0e3
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-20 08:54:05 -0500

    Add RecordDemo

---
+++ CSharp/RecordDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace RecordDemo;
+
+class Program
+{
+    record Person(string Name, int Age);
+    static void Main()
+    {
+        Person person1 = new("Alice", 30);
+        Person person2 = new("Alice", 30);
+
+        //structural equality
+        if (person1 == person2)
+        {
+            Console.WriteLine("Both persons are equal.");
+        }
+        else
+        {
+            Console.WriteLine("Persons are not equal.");
+        }
+
+        // reference equality
+        if (ReferenceEquals(person1, person2))
+        {
+            Console.WriteLine("Both persons refer to the same instance.");
+        }
+        else
+        {
+            Console.WriteLine("Persons refer to different instances.");
+        }
+    }
+}
