5f1721c49cd3e7165c42bb8b95cbad1fb0ca1675
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-20 10:15:55 -0500

    Add HashTableDemo

---
+++ CSharp/HashTableDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+namespace HashTableDemo;
+
+class Program
+{
+    static void Main()
+    {
+        Hashtable table = new Hashtable();
+
+        table["age"] = 30;
+        table["price"] = 19.99;
+        table["status"] = true;
+
+        table.Add("type", "vintage");
+
+        int age = (int)table["age"];
+        double price = (double)table["price"];
+        bool status = (bool)table["status"];
+
+        System.Console.WriteLine($"Age: {age}");
+        System.Console.WriteLine($"Price: {price}");
+        System.Console.WriteLine($"Status: {status}");
+        System.Console.WriteLine($"Type: {table["type"]}");
+    }
+}
