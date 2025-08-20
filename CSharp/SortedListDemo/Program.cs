0608e1726c4059c8dfdc0286b847bc9fa9850837
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-20 11:10:04 -0500

    Add SortedListDemo

---
+++ CSharp/SortedListDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace SortedListDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SortedList productPrices = new SortedList();
+
+        productPrices.Add(102, 19.99);
+        productPrices.Add(101, 6.99);
+        productPrices.Add(103, 14.99);
+
+        Console.WriteLine("Product prices (sorted by key):");
+        foreach (DictionaryEntry entry in productPrices)
+        {
+            Console.WriteLine($"{entry.Key}: ${entry.Value}");
+        }
+
+        double price = (double)productPrices[102];
+        Console.WriteLine($"\nPrice of product 102: ${price}");
+
+    }
+}
