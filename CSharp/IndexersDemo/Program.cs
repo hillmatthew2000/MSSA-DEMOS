f01614bf03223d24a06009eded3a22802ab7134a
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-25 10:05:18 -0500

    Update IndexersDemo

--- CSharp/IndexersDemo/Program.cs
+++ CSharp/IndexersDemo/Program.cs
@@ -72,3 +72,25 @@ class Library
     public int Count => books.Count;
 
 }
+
+public class BaseballTeam
+    {
+        private string[] players = new string[9];
+        private readonly List<string> positionAbbreviations = new List<string>
+        {
+            "P", "C", "1B", "2B", "3B", "SS", "LF", "CF", "RF"
+        };
+ 
+        public string this[int position]
+        {
+            get { return players[position - 1]; }
+            set { players[position - 1] = value; }
+        }
+ 
+        public string this[string position]
+        {
+            get { return players[positionAbbreviations.IndexOf(position)]; }
+            set { players[positionAbbreviations.IndexOf(position)] = value; }
+        }
+ 
+    }
