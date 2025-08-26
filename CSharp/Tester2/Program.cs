229093f96c0519fb332449bc7dd1bb23fa5b61d4
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-26 08:32:08 -0500

    Update Tester2

--- CSharp/Tester2/Program.cs
+++ CSharp/Tester2/Program.cs
@@ -1,39 +1,10 @@
-﻿using System.Reflection;
-
-namespace tester2;
+﻿namespace tester2;
 
 class Program
 
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Start of Main");
-
-        try
-        {
-            Console.WriteLine("Entered outer try block");
-            try
-            {
-                Console.WriteLine("Entered inner try block");
-
-                int x = 0;
-                int result = 10 / x;
-
-                Console.WriteLine("This line will not be executed due to exception above.");
-            }
-            finally
-            {
-                Console.WriteLine("Inner finally block executed.");
-            }
-        }
-        catch (DivideByZeroException)
-        {
-            Console.WriteLine("Caught DividByZeroException in outer catch block.");
-        }
-        finally
-        {
-            Console.WriteLine("Outer finally block executed.");
-        }
-        Console.WriteLine("End of Main");
+        
     }
 }
