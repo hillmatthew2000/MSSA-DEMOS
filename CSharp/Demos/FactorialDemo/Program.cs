4c70f69442cf6d4000cdbef9847cbf4ab9aef1fc
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-09-08 11:26:55 -0500

    Add Factorial Demo

---
+++ CSharp/Demos/FactorialDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FactorialDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int fact, num;
+        Console.WriteLine("Enter a number to find factorial");
+        num = Convert.ToInt32(Console.ReadLine());
+        fact = factorial(num);
+        Console.WriteLine($"Factorial of {num} is {fact}");
+    }
+
+    public static int factorial(int num)
+    {
+        if (num == 0)
+            return 1;
+        else
+            return num * factorial(num - 1);
+    }
+}
--- CSharp/Tester2/Program.cs
+++ CSharp/Tester2/Program.cs
@@ -3,10 +3,8 @@
 class Program 
 
 {
-
     static void Main()
     {
-        
-    }
 
+    }
 }
