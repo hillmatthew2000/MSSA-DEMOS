642f95d24289b0c8c1f8708a0f85f349a2c9d489
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-09-10 16:02:07 -0500

    Update Linked List

--- CSharp/Data Structures/LinkedList/Program.cs
+++ CSharp/Data Structures/LinkedList/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LinkedList;
+﻿using System.Text;
+
+namespace LinkedList;
 
 class Program
 {
@@ -44,8 +46,12 @@ class Program
 
     public class SingleLinkedListOfIntegers
     {
-        IntNode Head;
-        IntNode Tail;
+        //head
+        public IntNode? Head { get; private set; }
+        //tail
+        public IntNode? Tail { get; private set; }
+
+        //count
         int count = 0;
 
         public SingleLinkedListOfIntegers()
@@ -55,8 +61,7 @@ class Program
             count = 0;
         }
 
-        public int Count => count;
-
+        //Insert Head
         public void AddFirst(int data)
         {
             IntNode newNode = new IntNode(data);
@@ -72,120 +77,169 @@ class Program
             count++;
         }
 
+        //Insert Tail
         public void AddLast(int data)
         {
+            //todo: make it work
             IntNode newNode = new IntNode(data);
             if (Tail == null)
             {
                 Head = newNode;
-                Tail = newNode;
             }
             else
             {
-            Tail.Next = newNode;
-            Tail = newNode;
+                Tail.Next = newNode;
             }
+            Tail = newNode;
             count++;
         }
 
+        //remove head
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown if the list is empty</exception>
         public void RemoveFirst()
         {
             if (Head == null)
             {
                 throw new InvalidOperationException("The list is empty.");
             }
-            //check for one node
+
+            // only one node?
             if (Head.Next == null)
             {
                 Head = null;
                 Tail = null;
-                count--;
-                return;
             }
-            count--;
-        }
-
-        public void RemoveLast()
-        {
-            if (Head == null) return;
-            if (Head.Next == null)
-            {
-                Head = null;
-                return;
-            }
-
-            IntNode current = Head;
-            while (current.Next.Next != null)
-            {
-                current = current.Next;
-            }
-            current.Next = null;
-        }
-
-        public void AddAfter(IntNode target, int data)
-        {
-            IntNode newNode = new IntNode(data);
-            newNode.Next = target.Next;
-            target.Next = newNode;
-        }
-
-        public void AddAfter(int target, int data)
-        {
-            IntNode? current = Head;
-            while (current != null)
-            {
-                if (current.Data == target)
-                {
-                    AddAfter(current, data);
-                    return;
-                }
-                current = current.Next;
-            }
-        }
-
-        public void RemoveNode(int targetData)
-        {
-            if (Head == null) return;
-            if (Head.Data == targetData)
+            else
             {
                 Head = Head.Next;
-                return;
+            }
+            count--;
+
+        }
+
+        //remove tail
+        public void RemoveLast()
+        {
+            if (Head == null)
+            {
+                throw new InvalidOperationException("The List is empty.Again");
+            }
+            //only 1
+            if (Head?.Next == null)
+            {
+                Head = null;
+                Tail = null;
+            }
+            //multiple
+            else
+            {
+                //addLast(20), addLast(30), addLast(40)
+                //tail should be 30
+                //head should be 20
+                IntNode current = Head;
+                while (current.Next != null && current.Next != Tail)
+                {
+                    current = current.Next;
+                }
+                current.Next = null;
+                Tail = current;
+            }
+            count--;
+
+        }
+
+        //Insert before
+        public void AddBefore(int targetData, int newData)
+        {
+            // test if head is null
+            if (Head == null)
+            {
+                throw new InvalidOperationException("The List is empty. Fired");
             }
 
-            IntNode? current = Head;
-            while (current.Next != null)
+            //test if head = target
+            if (Head.Data == targetData)
             {
-                if (current.Next.Data == targetData)
-                {
-                    current.Next = current.Next.Next;
-                    return;
-                }
+                AddFirst(newData);
+            }
+
+            //set current to head
+            //traverse list to find the node where targetData is in the next node
+            IntNode current = Head;
+            while (current.Next != null && current.Next.Data != targetData)
+            {
                 current = current.Next;
             }
+            if (current.Next == null)
+            {
+                throw new InvalidOperationException("Target data not found. Fired");
+            }
+            IntNode newNode = new IntNode(newData);
+            newNode.Next = current.Next;
+            current.Next = newNode;
         }
+
+        //Insert after
+        public void AddAfter(int targetData, int newData)
+        {
+
+        }
+
+        //remove any node
+        public void RemoveNode(int targetData)
+        {
+
+        }
+
+        public void RemoveBefore(int targetData)
+        {
+
+        }
+
+        public void RemoveAfter(int targetData)
+        {
+
+        }
+
+
+        //what is the size (readonly)
+        public int Count => count;
+
+        // public int Count
+        // {
+        //     get
+        //     {
+        //         return count;
+        //     }
+        // }
 
         public IntNode Find(int targetData)
         {
-            IntNode? current = Head;
-            while (current != null)
-            {
-                if (current.Data == targetData)
-                {
-                    return current;
-                }
-                current = current.Next;
-            }
-            return null;
+            //they pass in data to find  (i.e. 7)
+            //a) does it exist? (does 7 exist somewhere in the list)
+            //b) if not, what to do? (throw an exception or return null?)
+            //c) if so , what to do? (return the found node)
+            throw new NotImplementedException("Write the find method");
         }
 
-        public void PrintList()
+
+        public string PrintList()
         {
+            //use a stringbuilder
+            StringBuilder sb = new StringBuilder();
+
             IntNode? current = Head;
             while (current != null)
             {
-                Console.WriteLine(current.Data);
+                sb.Append(current.Data + " ");
                 current = current.Next;
             }
+
+            //return the stringbuilder to string
+            return sb.ToString();
         }
     }
 }
