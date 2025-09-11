59bffc1cb13ce8316b087a32d27ca12844643af4
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-09-11 16:10:46 -0500

    Add Priority Queue

---
+++ CSharp/Data Structures/PriorityQueue/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PriorityQueue;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PriorityQueue<string, int> pq = new PriorityQueue<string, int>();
+
+        pq.Enqueue("Task 1", 40);
+        pq.Enqueue("Task 2", 30);
+        pq.Enqueue("Task 3", 20);
+        pq.Enqueue("Task 4", 10);
+        pq.Enqueue("Task 5", 50);
+
+        while (pq.TryDequeue(out string? item, out int priority))
+        {
+            Console.WriteLine($"The priority of the programming language {item} is {priority} ");
+        }
+    }
+}
