a52954263e4caed29ff56a164d55349192d0c26d
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-09-23 13:00:42 -0500

    Add Method Injection demo

---
+++ CSharp/Demos/MethodInjection/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MethodInjection;
+
+class Program
+{
+    static void Main()
+    {
+        var ReportService = new ReportService();
+        ILogger logger = new Logger();
+        ReportService.GenerateReport("Sales data Q1", logger);
+        ReportService.GenerateReport("Sales data Q2", logger);
+    }
+}
+
+public class ReportService
+{
+    public void GenerateReport(string data, ILogger logger)
+    {
+        if (logger is null)
+            throw new ArgumentNullException(nameof(logger));
+            
+        logger.Log("Report generation started");
+        logger.Log($"Generating report with data: {data}");
+        logger.Log("Report generation completed");
+    }
+}
+
+public interface ILogger
+{
+    void Log(string message);
+}
+public class Logger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"Log: {message}");
+    }
+}
