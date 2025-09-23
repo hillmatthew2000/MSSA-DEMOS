95d9e7feede06c31cb3a6debf66d75e2adcc3ecf
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-09-23 14:39:56 -0500

    Add Property Injection

---
+++ CSharp/Demos/PropertyInjection/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace PropertyInjection;
+
+public class NotificationService
+{
+    public IMessageService MessageService { get; set;  }
+ 
+    public void SendNotification(string message)
+    {
+        if (MessageService == null)
+            throw new InvalidOperationException("Message service dependency not provided");
+        MessageService.Send(message);
+    }
+}
+ 
+public interface IMessageService
+{
+    void Send(string message);
+}
+public class SmsService : IMessageService
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"SMS sent: {message}");
+    }
+}
+ 
+public class MockService: IMessageService
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Mock sent: {message}");
+    }
+}
+ 
+class Program
+{
+    static void Main()
+    {
+        var notifier = new NotificationService();
+ 
+        notifier.MessageService = new SmsService();
+        notifier.SendNotification("Hello Alice");
+ 
+        notifier.MessageService = new MockService();
+        notifier.SendNotification("Hello Bob");
+    }
+}
