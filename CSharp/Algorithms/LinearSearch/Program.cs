c155a8dbad8275ee68202c43f2706a8d42d1295a
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-09-30 08:35:40 -0500

    Add Linear Search Algo

---
+++ CSharp/Algorithms/LinearSearch/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LinearSearch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 10, 2, 34, 4, 5, 15, 23 };
+        int target = 15;
+        int result = LinearSearch(arr, target);
+        if (result != -1)
+        {
+            Console.WriteLine($"Element found at index: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Element not found in the array.");
+        }
+    }
+
+    static int LinearSearch(int[] arr, int target)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
