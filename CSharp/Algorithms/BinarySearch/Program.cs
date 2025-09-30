726699a1d0b458e4026969e21a0eeb49b9409d93
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-09-30 08:35:16 -0500

    Add Binary Search Algo

---
+++ CSharp/Algorithms/BinarySearch/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace BinarySearch;
+
+class Program
+{
+    static void Main()
+    {
+        int[] isbnNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int target = GetUserInput();
+        DisplaySearchResults(BinarySearch(isbnNumbers, target));
+
+
+    }
+
+    static int BinarySearch(int[] nums, int target)
+    {
+        int lowerBound = 0;
+        int upperBound = nums.Length - 1;
+        int mid = (upperBound + lowerBound) / 2;
+        while (lowerBound <= upperBound)
+        {
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+            {
+                upperBound = mid - 1;
+                mid = (upperBound + lowerBound) / 2;
+            }
+            else if (nums[mid] < target)
+            {
+                lowerBound = mid + 1;
+                mid = (upperBound + lowerBound) / 2;
+            }
+        }
+        return -1;
+    }
+
+    static void DisplaySearchResults(int result)
+    {
+        if (result == -1)
+            Console.WriteLine("ISBN not found");
+        else
+            Console.WriteLine($"ISBN number found at index: {result}");
+    }
+
+    static int GetUserInput()
+    {
+        Console.WriteLine("Enter a ISBN number to search for.");
+
+        bool isValid = int.TryParse(Console.ReadLine(), out int value);
+        if (!isValid) throw new InvalidExpressionException("Invalid input");
+        return value;      
+    }
+}
